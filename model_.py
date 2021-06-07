eb710d32280955f9ce5c3331c4cf68b91fc3b8b4
Sergey Anufriev <anufriiev.sergii@gmail.com>
2021-06-07 12:29:04 +0300

    add h_0,c_0 arguments to make sampling with h_0!=0 and c_0 !=0 possible

--- model_.py
+++ model_.py
@@ -1,8 +1,9 @@
+import torch
 import torch.nn as nn
 
 
 class RNN_forward(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout,device):
         super().__init__()
 
         self.hid_dim   = hid_dim
@@ -12,18 +13,31 @@ class RNN_forward(nn.Module):
         self.dropout   = nn.Dropout(dropout)
         self.linear    = nn.Linear(hid_dim, input_dim)
 
-    def forward(self, src):
-        '''Input: input seq size = [batch * seq_len]'''
+
+        self.device = device
+
+    def forward(self,src,hidden):
+        '''Input: input
+        1) sequence with size = [batch * seq_len]
+        2) hidden state (h_0,c_0)'''
 
         embedded = self.dropout(self.embedding(src))
         '''Embed size = [batch * seq_len * embed_dim]'''
 
         '''Change embedded size to [seq_len * batch * embed_dim] (for RNN)
                      Output size = [seq_len * batch * hid_dim]'''
-        outputs, (h_n, c_n) = self.rnn(embedded.permute(1, 0, 2))
+        outputs, (h_n, c_n) = self.rnn(embedded.permute(1, 0, 2),hidden)
 
         '''Apply linear layer to RNN outputs
         prediction size = [seq_len * batch * vocab_size ]'''
         prediction = self.linear(outputs.squeeze(0))
 
         return prediction, h_n, c_n
+
+    def init_hidden_(self,batch_size):
+        '''Initialise hidden and cell states'''
+
+        h_0 = torch.zeros([self.n_layers,batch_size,self.n_hidden],device=self.device)
+        c_0 = torch.zeros([self.n_layers,batch_size,self.n_hidden],device=self.device)
+
+        return h_0,c_0
