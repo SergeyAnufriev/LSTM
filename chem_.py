069d474ec1072422518919aa6a9e8ee3dc0d0a44
Sergey Anufriev <anufriiev.sergii@gmail.com>
2021-06-17 12:49:27 +0300

    sample by batch

--- chem_.py
+++ chem_.py
@@ -1,6 +1,27 @@
 from rdkit import Chem
+import numpy as np
 
 def substructure(mol,fragment):
     return mol.GetSubstructMatches(Chem.MolFromSmiles(fragment))
+
+def valid_(smiles):
+  try:
+      Chem.SanitizeMol(Chem.MolFromSmiles(smiles))
+      return 1
+  except:
+    return 0
+
+
+def top_k_smiles(smiles,func,k=64):
+    '''Input
+    1) Smiles - list of smiles strings
+    2) func - function which assigns score to a smiles string
+    3) k - top K smiles by arg max func (smiles)
+    Return: top K smiles list'''
+
+    R = np.array([func(x) for x in smiles])
+    indx = np.argsort(R)[-k:]
+    return [smiles[x] for x in indx]
+
 
 
